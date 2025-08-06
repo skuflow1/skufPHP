ea8d1a515acf5222ba79f1fb915fe8276f7a5d50
Skuf <skuflow1@gmail.com>
2025-08-06 16:03:04 +0200

    Create recsearch.php

---
+++ recsearch.php
@@ -0,0 +1,12 @@
+Minh1234@a<?php
+function generatePassword($length = 8) {
+    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+    $password = "";
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $chars[random_int(0, strlen($chars) - 1)];
+    }
+    return $password;
+}
+
+echo "Сгенерированный пароль: " . generatePassword(12);
+?>
